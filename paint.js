000d53be4230830c7c048c66bfaec3cd7fe4e6fd
Duda Nunes <eduardapmn@gmail.com>
2023-02-08 23:50:59 -0300

    Eye Drop Tool First Try
    Não terminei ainda, estou tendo problema na hora de remover o event listener do PixelPaint que está pintando antes de eu pegar a cor com o dropper

--- paint.js
+++ paint.js
@@ -1,6 +1,8 @@
+var color = ''
 size(3)
 
 function size(btnId){
+    console.log('criou o canvas')
 
     let canvas = document.querySelector('.pixel-canvas')
 
@@ -14,29 +16,79 @@ function size(btnId){
     }
     canvas.style.cssText+='width:'+w+'vh; height:'+w+'vh; grid-template-columns:repeat('+x+',1fr);grid-template-rows:repeat('+x+',1fr);'
     pixel = document.querySelectorAll('.pixel-paint')
-    paint() 
+
+    paint()
 }
 
-function paint(){
+function paint(remove){
 
-    let pixel = document.querySelectorAll('.pixel-paint')
-    let colorPicker = document.querySelectorAll('.color')
-    let color = ''
+    if (remove == true){
+
+        console.log('Rodou o Paint() no if')
+
+        for(var i = 0; i<pixel.length; i++){
+            pixel[i].removeEventListener('click',pixelPaint)
+            console.log('Pixel Paint Removido')
+        }
+    }
+
+    else{
+
+        console.log('Rodou o Paint () no else')
+
+        let pixel = document.querySelectorAll('.pixel-paint')
+        let colorPicker = document.querySelectorAll('.color')
+
+        for(var i = 0; i<pixel.length; i++){
+            pixel[i].addEventListener('click',pixelPaint)
+            console.log('PixelPaint EventListener')
+        }
+
+        for(var c = 0; c<colorPicker.length; c++){
+            colorPicker[c].addEventListener('click', paintBrush)
+            colorPicker[c].addEventListener('input', paintBrush)
+        }
+
+        function paintBrush(colorPicked){
+            color = colorPicked.target.value
+        }
+
+        function pixelPaint(){
+            console.log('Pixel Paint Pintou')
+            this.style.background = color;
+        }
+    }
+}
+
+function eraser(){
+    console.log('borracha')
+    color = "#f4f1de"
+    paint()
+}
+
+function dropper(){
+    console.log('Clicou no Dropper')
+
+    remove = true;
+
+    paint(remove)
 
     for(var i = 0; i<pixel.length; i++){
-        pixel[i].addEventListener('click', pixelPaint)
-    }
+        pixel[i].addEventListener('click', getColor)
+        console.log("Dropper EventListener ")
+    }  
 
-    for(var c = 0; c<colorPicker.length; c++){
-        colorPicker[c].addEventListener('click', paintBrush)
-        colorPicker[c].addEventListener('input', paintBrush)
-    }
+    function getColor(){
 
-    function paintBrush(colorPicked){
-        color = colorPicked.target.value
-    }
+        console.log('Rodou getColor()')
 
-    function pixelPaint(){
-        this.style.background = color;
+        for(var i = 0; i<pixel.length; i++){
+            pixel[i].removeEventListener('click', getColor)
+            console.log("Dropper Remove EventListener")
+        } 
+        
+        color = window.getComputedStyle(this).backgroundColor;
+        console.log("Dropper Pegou:  " + color)
     }
+        
 }
